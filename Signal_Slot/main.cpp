74ec0fe6d6eda6d8a4d046c96e2ed760f6330f45
Life9Owner <1349615576@qq.com>
2023-10-20 20:54:37 +0800

    third

--- Signal_Slot/main.cpp
+++ Signal_Slot/main.cpp
@@ -1,12 +1,23 @@
 #include <QCoreApplication>
 #include"sender.h"
+#include"receiver.h"
 int main(int argc, char *argv[])
 {
     Sender senderObj;
+    Receiver receiverObj;
     senderObj.incAge();
-    qDebug()<<senderObj.get_m_age();
+//    qDebug()<<senderObj.get_m_age();
+//    senderObj.incAge();
+//    senderObj.incAge();
+//    qDebug()<<senderObj.get_m_age();
+
+
+    QObject::connect(&senderObj,&Sender::ageChanged,&receiverObj,&Receiver::ageChanged);
     senderObj.incAge();
     senderObj.incAge();
-    qDebug()<<senderObj.get_m_age();
+    receiverObj.~Receiver();
+//    QObject::disconnect(&senderObj,&Sender::ageChanged,&receiverObj,&Receiver::ageChanged);
+
+    senderObj.incAge();
 
 }
